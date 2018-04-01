32645db99b2da81c1cdb72b08a408b5f1a854462
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2018-04-01 20:43:25 +0200

    profiles/mask-alt-pkgs: remove support for `all`
    portage doesn't support ::repo syntax in repo profiles

--- profiles/mask-alt-pkgs/regenerate.sh
+++ profiles/mask-alt-pkgs/regenerate.sh
@@ -4,7 +4,6 @@ set -xvue
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-printf "" > "${SCRIPT_DIR}/all/parent"
 
 for d in "${SCRIPT_DIR}"/* ; do
 	if ! [[ -d "${d}" ]] ; then
@@ -13,13 +12,6 @@ for d in "${SCRIPT_DIR}"/* ; do
 	fi
 
 	repo_name="$(basename "${d}")"
-
-	if [[ "${repo_name}" == "all" ]] ; then
-		echo "repo_name == 'all' -> skipping"
-		continue
-	fi
-
-	echo "../${repo_name}" >> "${SCRIPT_DIR}/all/parent"
 
 	EIX_LIMIT=0 eix --in-overlay rindeal --and --in-overlay "${repo_name}" --only-names | sed "s|$|::${repo_name}|" > "${d}/package.mask"
 done
