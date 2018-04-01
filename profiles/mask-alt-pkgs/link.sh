47222fe941cb61956b674a69afb3a97a32849bcc
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2018-04-01 20:52:49 +0200

    profiles/mask-alt-pkgs: add `link.sh` script

---
+++ profiles/mask-alt-pkgs/link.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# set -vx
+set -ue
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if (( $# != 1 )) ; then
+	echo "Usage: $0 DST_DIR"
+	echo ""
+	echo "Example:"
+	echo ""
+	echo "    $0 /etc/portage/package.mask/rindeal-mask-alt-pkgs"
+	echo ""
+	exit 1
+fi
+
+DST_DIR="${1}"
+
+if ! [[ -d "${DST_DIR}" ]] ; then
+	echo "DST_DIR is not a directory"
+	exit 1
+fi
+
+for d in "${SCRIPT_DIR}"/* ; do
+	if ! [[ -d "${d}" ]] ; then
+		echo "'${d}' is not a directory -> skipping"
+		continue
+	fi
+
+	pmask_file="${d}/package.mask"
+	repo_name="$(basename "${d}")"
+
+	ln -vfs "${pmask_file}" "${DST_DIR}/${repo_name}"
+done
