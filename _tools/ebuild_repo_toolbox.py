25ab94b438bd04c26e454175479970e4d57b340e
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2018-04-04 00:59:50 +0200

    _tools/ebuild_repo_toolbox.py: update

--- _tools/ebuild_repo_toolbox.py
+++ _tools/ebuild_repo_toolbox.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-# Copyright 2016-2017 Jan Chren (rindeal) <dev.rindeal@gmail.com>
+# Copyright 2016-2018 Jan Chren (rindeal) <dev.rindeal@gmail.com>
 # Distributed under the terms of the GNU General Public License v2
 
 import os
+
 import portage
+import portage.dbapi
+import portage.dbapi.porttree
+import portage.dbapi.vartree
+
 
 class EbuildRepoToolbox:
 
@@ -15,7 +20,8 @@ class EbuildRepoToolbox:
 			with os.scandir(d) as entries:
 				for entry in entries:
 					if entry.is_dir() and entry.name == "profiles":
-						if os.path.exists(os.path.join(entry.path, "repo_name")):
+						repo_name_path = os.path.join(entry.path, "repo_name")
+						if os.path.exists(repo_name_path):
 							return d
 			d = os.path.dirname(d)
 
@@ -29,8 +35,14 @@ class EbuildRepoToolbox:
 		os.chdir(repo_dir)
 		os.environ["PORTDIR_OVERLAY"] = repo_dir
 
-	@staticmethod
-	def get_portage_dbapi():
-		return portage.db[portage.root]["porttree"].dbapi
+	@classmethod
+	def get_db(cls) -> dict:
+		return portage.db[portage.root]
 
-	#@staticmethod
+	@classmethod
+	def get_portagetree(cls) -> portage.dbapi.porttree.portagetree:
+		return cls.get_db()['porttree']
+
+	@classmethod
+	def get_vartree(cls) -> portage.dbapi.vartree.vartree:
+		return cls.get_db()['vartree']
